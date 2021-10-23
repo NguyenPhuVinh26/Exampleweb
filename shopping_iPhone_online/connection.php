5a343aca3a0da528f9ab14faf19d3d4fedf03c8f
NguyenPhuVinh26 <91648114+NguyenPhuVinh26@users.noreply.github.com>
2021-10-23 08:19:08 +0700

    .

--- shopping_iPhone_online/connection.php
+++ shopping_iPhone_online/connection.php
@@ -1,3 +1,15 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'shoping_online') or die("Can not connect database".mysqli_connect_error());
+    //$conn = mysqli_connect('localhost', 'root', '', 'shoping_online') or die("Can not connect database".mysqli_connect_error());
+	/*$Connect = mysqli_connect("localhost","root","","qlbh") or die("Lỗi".mysqli_error($Connect));
+	
+	mysqli_query($Connect,'SET NAMES "utf8"');
+	//mysqli_close($Connect);*/
+	$Connect = pg_connect("postgres://irazhjycrycebu:2958d802a2f10daccbcfcd5c88fc3afadc7dbf9c6893d8ec969cff97ee7ae480@ec2-18-234-15-247.compute-1.amazonaws.com:5432/dc83ouue9v2ndu");
+    //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
+	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
+	
+    if (!$Connect) {
+        die("Connection failed");
+    }
+
 ?>
